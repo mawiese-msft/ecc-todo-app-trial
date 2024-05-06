787cdf3e230d8a05e4e3c906d057ffa660a5a30e
Maximilian Wiese <mawiese@microsoft.com>
2024-05-06 14:29:53 +0000

    simple step 0 todo app

---
+++ TODOAPP/app.py
@@ -0,0 +1,8 @@
+todo_list = []
+
+todo_list.append("Buy milk")
+todo_list.append("Learn more about Azure")
+todo_list.append("Complete first Python project")
+
+for todo in todo_list:
+    print(todo)
