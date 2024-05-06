9d6da093bee507f977e117fc160395a705306243
Maximilian Wiese <mawiese@microsoft.com>
2024-05-06 21:14:46 +0000

    Fixed path to txt file

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 todo_list = []
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) #My txt file is not in the same directory thus i needed to add an extra "dirname"
 todo_file = os.path.join(basedir, 'todo_list.txt')
 
 #Load the to-do list from file before moving on. 
