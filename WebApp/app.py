92c240a9164d35aa01ed0ead942581ff402d5b3a
Maximilian Wiese <mawiese@microsoft.com>
2024-05-06 21:49:39 +0000

    Added bootstrap

--- WebApp/app.py
+++ WebApp/app.py
@@ -16,11 +16,35 @@ try:
 except FileNotFoundError:
     pass
 
+#Http endpoints
 @app.route("/")
 def index():
     with open(todo_file, "r") as file:
         todo_list = file.read().splitlines()
     return render_template("index.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+    todo = request.form["todo"]
+    todo_list.append(todo)
+    save_todo_list()
+    return redirect(url_for("index"))
+
+@app.route("/remove", methods=["POST"])
+def remove_todo():
+    item_number = int(request.form["item_number"])
+    if 0 < item_number <= len(todo_list):
+        todo_list.pop(item_number - 1)
+        save_todo_list()
+    return redirect(url_for("index"))
+
+
+#Save to file method
+def save_todo_list():
+    with open("todo_list.txt", "w") as file:
+        for todo in todo_list:
+            file.write(todo + "\n")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
